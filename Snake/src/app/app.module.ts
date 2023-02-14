64ab9f7f71dec743acae9f5d131f1009d75cd544
Kamil <k_markowski94@o2.pl>
2023-02-14 16:15:43 +0100

    first commit

---
+++ Snake/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from  '@angular/core';
+
+import {AppComponent} from './app.component';
+import {NgxSnakeModule} from 'ngx-snake';
+import { StartPageComponent } from './start-page/start-page.component';
+import { GamePageComponent } from './game-page/game-page.component';
+import { FormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+      AppComponent,
+      StartPageComponent,
+      GamePageComponent,
+  ],
+  imports: [
+      BrowserModule,
+      NgxSnakeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
+
---
+++ Snake/src/app/start-page/start-page.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+
+interface FormData {
+  name: string;
+  email: string;
+}
+
+@Component({
+  selector: 'app-start-page',
+  templateUrl: './start-page.component.html',
+  styleUrls: ['./start-page.component.css']
+})
+export class StartPageComponent {
+
+  public onSubmit(data: FormData) {
+    console.log(data);
+  }
+}
